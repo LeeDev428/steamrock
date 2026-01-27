9f881a556cade7375ee458485e878fd65bd4e1ba
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-28 05:29:48 +0800

    Create Home.jsx

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Hero from '../components/Hero';
+import Features from '../components/Features';
+import FeaturedProperties from '../components/FeaturedProperties';
+import ContactForm from '../components/ContactForm';
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <Features />
+      <FeaturedProperties />
+      <ContactForm />
+    </div>
+  );
+};
+
+export default Home;
