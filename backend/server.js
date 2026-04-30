deb907643cfd4ea7ff25854fba5bdd573bda2f7f
Lee Torres <grafrafraftorres28@gmail.com>
2026-04-30 23:01:13 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 const dotenv = require('dotenv');
 const path = require('path');
 const http = require('http');
